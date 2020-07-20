18140202507b7fa36fd4856583c593f001faacbf
Girish <girish.potnuru@otis.com>
2020-07-20 08:13:20 +0530

    Initial Commit

---
+++ GitDemo/GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Girish on 20/07/20.
+//  Copyright © 2020 Girish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
