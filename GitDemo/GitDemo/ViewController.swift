ee53a3ced450c75375e663319d86e0a883e9dc8d
Girish <girish.potnuru@otis.com>
2020-07-22 07:53:34 +0530

    commit for add and commit

--- GitDemo/GitDemo/ViewController.swift
+++ GitDemo/GitDemo/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Commented code
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
